745f1a7cc3e2c478bddf483ed17b6a0d51a4fa42
readypeng <38313898+readypeng@users.noreply.github.com>
2020-09-02 21:42:52 +0800

    Confusion Matrix for Classification

--- Main.m
+++ Main.m
@@ -3,7 +3,7 @@
 addpath 'wfdb-app-toolbox-0-10-0\mcode'
 clear; clc; close all;
 
-%% Read in and stack using for-loop
+%% Read in and construct data to stack using for-loop
 X_stack = [];
 Y_stack = [];
 
@@ -57,6 +57,48 @@ success_rate = sum(correct_mark) / length(label)
 % 
 rloss = resubLoss(mdl)
 
+%% Classification Model 
+% Classification Model can be generated using 'Classification Learner'
+% Evaluate confusion matrix & Learner using Classificaton Learner
+
+%% LEGACY 
+% % Evaluate/Comparison Classification Model - Confusion Matrix
+% % Randomize the measurements and groups in the data.
+% rng(0,'twister'); % For reproducibility
+% numObs = height(X_stack);
+% p = randperm(numObs);
+% X_stack = X_stack(p,:);
+% Y_stack = Y_stack(p);
+% 
+% % Train a classifier by using measurements in the first portion of the data.
+% portion_mark = floor(numObs/30);
+% training = X_stack(1:portion_mark,:);
+% trainingSpecies = Y_stack(1:portion_mark);
+% %-----------Change Model BELOW----------------
+% Mdl = fitcdiscr(training, trainingSpecies);
+% 
+% %-----------Change Model ABOVE---------------
+% 
+% % Predict labels for the measurements in the second half of the data by using the trained classifier.
+% sample = X_stack(portion_mark+1:end,:);
+% grouphat = predict(Mdl,sample);
+% 
+% % Specify the group order and display the confusion matrix for the resulting classification.
+% group = Y_stack(portion_mark+1:end);
+% [C,order] = confusionmat(group,grouphat);
+% cm = confusionchart(grouphat,group)
+
+%% TODO: Evaluate Classification Model - Cross Validation
+X = X_stack;
+Y = num2cell(Y_stack);
+cp = classperf(Y)
+for i = 1:10
+    [train,test] = crossvalind('LeaveMOut',Y,5);
+    mdl = fitcknn(X(train,:),Y(train),'NumNeighbors',3);
+    predictions = predict(mdl,X(test,:));
+    classperf(cp,predictions,test);
+end
+
 %% Prepare trained classification model for Coder/Conversion 
 % use above code to train model named 'mdl'
 % saveLearnerForCoder(mdl,'RF_model');
@@ -92,14 +134,14 @@ y = net(X_array'); % net is not a function, its just the 'net' we trained above
 perf = perform(net,y,Y_array_flag')
 
 % Saving the network weights and biases
-net.IW	
-net.LW	
-net.b
+net.IW;
+net.LW;
+net.b;
 
 % save network for coder
 genFunction(net, 'NN_model', 'MatrixOnly','yes');
 
-%% Get final prediction as 0, 1, 2, 3, rather than [1, 0, 0, 0], [0, 1, 0, 0], ...
+%% Get final prediction of NN as 0, 1, 2, 3, rather than [1, 0, 0, 0], [0, 1, 0, 0], ...
 % get result (4*14497-->1*14497)
 y_distance_to_1 = abs(y - ones(size(y)));
 y_predict = [];
