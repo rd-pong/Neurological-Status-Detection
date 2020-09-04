959a82ecc665307d3965c47d91d38d75c442a810
readypeng <38313898+readypeng@users.noreply.github.com>
2020-09-04 19:49:13 +0800

    Final update at closing ceremony?

--- Main.m
+++ Main.m
@@ -37,15 +37,17 @@ XY = [X_stack array2table(Y_stack)];
 % Y_stack = [Y1; Y2; ...; Y19];
 % XY = [X_stack array2table(Y_stack)];
 
+%% %%%%%%%%%%%%%%%%%%%%Classification%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %% kNN Train & Test
 mdl = fitcknn(X_stack, Y_stack, 'NumNeighbors', 3, 'Standardize', 1);
-label = predict(mdl, [X20_AccTempEDA X20_SpO2HR(1:height(X20_AccTempEDA), :)]);
+% label = predict(mdl, [X20_AccTempEDA X20_SpO2HR(1:height(X20_AccTempEDA), :)]);
+label = predict(mdl, X_stack);
 
 % success rate count
 correct_mark = zeros(length(label), 1);
 for i = 1:length(label)
 
-    if label(i) == Y20(i)
+    if label(i) == Y_stack(i)
         correct_mark(i) = 1;
     else
         correct_mark(i) = 0;
@@ -54,39 +56,25 @@ for i = 1:length(label)
 end
 success_rate = sum(correct_mark) / length(label)
 
-% 
+% rloss below seems to equals 1-success_rate above
 rloss = resubLoss(mdl)
 
-%% Classification Model 
+%% Classification Model (use 'Classification Learner' app)
 % Classification Model can be generated using 'Classification Learner'
-% Evaluate confusion matrix & Learner using Classificaton Learner
+% Evaluate confusion matrix using Classificaton Learner
 
-%% LEGACY 
-% % Evaluate/Comparison Classification Model - Confusion Matrix
-% % Randomize the measurements and groups in the data.
-% rng(0,'twister'); % For reproducibility
-% numObs = height(X_stack);
-% p = randperm(numObs);
-% X_stack = X_stack(p,:);
-% Y_stack = Y_stack(p);
-% 
-% % Train a classifier by using measurements in the first portion of the data.
-% portion_mark = floor(numObs/30);
-% training = X_stack(1:portion_mark,:);
-% trainingSpecies = Y_stack(1:portion_mark);
-% %-----------Change Model BELOW----------------
-% Mdl = fitcdiscr(training, trainingSpecies);
-% 
-% %-----------Change Model ABOVE---------------
-% 
-% % Predict labels for the measurements in the second half of the data by using the trained classifier.
-% sample = X_stack(portion_mark+1:end,:);
-% grouphat = predict(Mdl,sample);
-% 
-% % Specify the group order and display the confusion matrix for the resulting classification.
-% group = Y_stack(portion_mark+1:end);
-% [C,order] = confusionmat(group,grouphat);
-% cm = confusionchart(grouphat,group)
+% Classification Model Using script
+
+% Accuracy using resubloss
+
+
+%% Prepare trained classification model for Coder/Conversion 
+% use above code to train model named 'mdl'
+% saveLearnerForCoder(mdl,'RF_model');
+
+% Classification Learning APP and export compact model, then call below
+% Refer to MATLAB doc: Code Generation and Classification Learner App
+saveLearnerForCoder(trainedModel.ClassificationEnsemble,'RF_model');
 
 %% TODO: Evaluate Classification Model - Cross Validation
 X = X_stack;
@@ -99,14 +87,7 @@ for i = 1:10
     classperf(cp,predictions,test);
 end
 
-%% Prepare trained classification model for Coder/Conversion 
-% use above code to train model named 'mdl'
-% saveLearnerForCoder(mdl,'RF_model');
-
-% Classification Learning APP and export compact model, then call below
-% Refer to MATLAB doc: Code Generation and Classification Learner App
-saveLearnerForCoder(trainedModel.ClassificationEnsemble,'RF_model');
-
+%% %%%%%%%%%%%%%%%%%%%%Neural Network%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %% Neural Network Train & Test
 % reorganize training data/label
 X_array = table2array(X_stack);
@@ -114,24 +95,25 @@ Y_array = Y_stack;
 Y_array_flag = [];
 for i = 1: length(Y_array)
     if Y_array(i) == 0
-        Y_array_flag = [[1 0 0 0]; Y_array_flag];
+        Y_array_flag = [Y_array_flag; [1 0 0 0]];
     elseif Y_array(i) == 1
-        Y_array_flag = [[0 1 0 0]; Y_array_flag];
+        Y_array_flag = [Y_array_flag; [0 1 0 0]];
     elseif Y_array(i) == 2
-        Y_array_flag = [[0 0 1 0]; Y_array_flag];
+        Y_array_flag = [Y_array_flag; [0 0 1 0]];
     elseif Y_array(i) == 3
-        Y_array_flag = [[0 0 0 1]; Y_array_flag];
+        Y_array_flag = [Y_array_flag; [0 0 0 1]];
     end
 end
 
 % design/train network
-net = fitnet(10,'traingd');
-view(net)
+net = fitnet(10,'traingdx');
+net.adaptParam.lr=0.3; % learning rate
+net.trainParam.mc = 0.2; % momentum
 [net,tr] = train(net, X_array', Y_array_flag');
 
 % test network
 y = net(X_array'); % net is not a function, its just the 'net' we trained above
-perf = perform(net,y,Y_array_flag')
+perf = perform(net,y,Y_array_flag');
 
 % Saving the network weights and biases
 net.IW;
@@ -139,15 +121,28 @@ net.LW;
 net.b;
 
 % save network for coder
-genFunction(net, 'NN_model', 'MatrixOnly','yes');
+% genFunction(net, 'NN_model', 'MatrixOnly','yes');
 
 %% Get final prediction of NN as 0, 1, 2, 3, rather than [1, 0, 0, 0], [0, 1, 0, 0], ...
 % get result (4*14497-->1*14497)
-y_distance_to_1 = abs(y - ones(size(y)));
-y_predict = [];
+Y_predict = [];
 for i = 1: length(y)
-    column_buff = y_distance_to_1(:, 1)
-    min_idx = find(column_buff == min(column_buff(:))) - 1;
-    y_predict = [min_idx y_predict];
+    column_buff = y(:, i);
+    max_idx = find(column_buff == max(column_buff(:)))-1;
+    Y_predict = [max_idx; Y_predict];
 end
 
+%% Confusion Matrix
+figure; plotconfusion(Y_array_flag', y);
+
+correct_mark = zeros(length(Y_stack), 1);
+for i = 1:length(Y_stack)
+
+    if Y_predict(i) == Y_stack(i)
+        correct_mark(i) = 1;
+    else
+        correct_mark(i) = 0;
+    end
+
+end
+success_rate = sum(correct_mark) / length(Y_stack)
